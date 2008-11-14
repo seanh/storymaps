e312da56ca99a902d72b18b714c390a7da946da3
Sean Hammond <seanh@sdf.lonestar.org>
2008-11-14 17:04:08 +0000

    Working on the StoryCard class, a class that composes a number of object-instances of PNode subclasses. So far: compose a rectangle from PPath, a PText, a PImage, and another PText, to create a card with a title, illustration and description.
    Added StoryCardDemo class as a unit demo for StoryCard.
    
    Added home.png, first image for demoing StoryCard.
    
    	modified:   StoryCard.java
    	new file:   StoryCardDemo.java
    	new file:   home.png

--- storymaps/StoryCard.java
+++ storymaps/StoryCard.java
@@ -1,67 +1,54 @@
 package storymaps;
-
-import edu.umd.cs.piccolo.nodes.PText;
+import java.awt.Color;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PImage;
-import java.util.Iterator;
-import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
-import java.awt.geom.Dimension2D;
-import edu.umd.cs.piccolo.event.PInputEvent;
-import edu.umd.cs.piccolo.PCamera;
+import edu.umd.cs.piccolo.nodes.PText;
 
-/**
- * A class for story cards.
- * 
- * StoryCard has to extend PNode then add another PNode (the layout node) to
- * itself then add further PNodes to the layout node, otherwise the drag event
- * listener doesn't work. No idea why it has to be this way (one unnecessary
- * level of node).
- * 
- * @author seanh
- */
-public class StoryCard extends PNode {
+public class StoryCard {
+    
+    /**
+     * The title that appears on this story card,
+     */
+    private String title;
+    
+    /**
+     * The description that appears on this story card.
+     */
+    private String description;
         
-    public StoryCard(final PCamera cam) {
-        super();
-        final PNode layout_node = new PNode() {
-            @Override
-            public void layoutChildren() {
-                double xOffset = 0;
-                double yOffset = 0;
-                Iterator i = getChildrenIterator();
-                while (i.hasNext()) {
-                    PNode each = (PNode) i.next();
-                    each.setOffset(xOffset, yOffset - each.getY());
-                    yOffset += each.getHeight();
-                }
-            }
-        };
-        addChild(layout_node);
-        PNode title = new PText("Villainy!");
-        layout_node.addChild(title);
-        PNode image = new PImage("/home/seanh/NetBeansProjects/PiccoloExample/build/classes/piccoloexample/villainy.png");
-        layout_node.addChild(image);
-        PNode desc = new PText("The villain commits some\nheinous act of villainy.");
-        layout_node.addChild(desc);
-        layout_node.addInputEventListener(new PBasicInputEventHandler() {
+    /**
+     * The root node of this StoryCard tree of nodes.
+     */
+    private PNode node;
+    
+    public StoryCard(String title, String description) {
+        this.title = title;
+        this.description = description;
+                
+        node = PPath.createRectangle(0, 0, 200, 240);
+        node.setPaint(Color.WHITE);
 
-            @Override
-            public void mouseClicked(PInputEvent event) {
-                if (event.getButton() == 1){
-                    cam.animateViewToCenterBounds(layout_node.getGlobalFullBounds(), true, 1000);
-                    event.setHandled(true);
-                }
-                else {
-                    event.setHandled(false);
-                }
-            }
-
-            @Override
-            public void mouseDragged(PInputEvent event) {
-                Dimension2D delta = event.getDeltaRelativeTo(layout_node);
-                layout_node.translate(delta.getWidth(), delta.getHeight());
-                event.setHandled(true);
-            }
-        });
+        PText title_node = new PText(title);
+        node.addChild(title_node);
+        title_node.setOffset(2,0);
+        title_node.setScale(2);
+                
+        PNode image_node = new PImage("/home/seanh/git/phd/storymaps_java/storymaps/home.png");
+        node.addChild(image_node);
+        image_node.setOffset(2,30);
+        
+        PText description_node = new PText(description);
+        node.addChild(description_node);
+        description_node.setOffset(2,120);
+        description_node.setWidth(196);
+        description_node.setConstrainWidthToTextWidth(false);
+        description_node.setBounds(0,0,196,100);
+                
+        node.setChildrenPickable(false);                        
+    }
+    
+    public PNode getNode() {
+        return node;
     }
 }
---
+++ storymaps/StoryCardDemo.java
@@ -0,0 +1,21 @@
+package storymaps;
+import edu.umd.cs.piccolox.PFrame;
+
+/**
+ * A demo class for the StoryCard class.
+ * 
+ * @author seanh
+ */
+public class StoryCardDemo extends PFrame {
+
+    // Override PFrame's initialize method to run the demo.
+    @Override
+    public void initialize() {
+        StoryCard card = new StoryCard("Villainy", "A villainous act by a villain, committed in a villainous way and, generally, in the spirit of villainy.");
+        getCanvas().getLayer().addChild(card.getNode());
+    }    
+
+    public static void main(String args[]) {
+        StoryCardDemo demo = new StoryCardDemo();
+    }    
+}
