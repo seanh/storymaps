e73873badb6b87713f07057128228fbb4688f0f7
Sean Hammond <seanh@sdf.lonestar.org>
2008-11-20 17:23:29 +0000

    Rewriting StoryMap and StoryMapDemo to follow the way StoryCards and StoryCardsDemo now work. Mostly done, but needs a careful look.

--- storymaps/StoryMap.java
+++ storymaps/StoryMap.java
@@ -1,38 +1,158 @@
 package storymaps;
 
 import DragAndDrop.*;
+import edu.umd.cs.piccolo.PNode;
+import java.awt.geom.Point2D;
 import java.util.HashSet;
 
-/**
- * The collection of story cards that the user can drag from.
- * 
- * @author seanh
- */
-public class StoryMap extends StoryBase
-                      implements DroppableOwner, DragDropObserver {
-    
+public class StoryMap  extends StoryBase implements DragDropObserver {
+
+    private HashSet<StoryCard> placeholders = new HashSet<StoryCard>();    
     private HashSet<StoryCard> storycards = new HashSet<StoryCard>();
-    
+                        
     public StoryMap(String title_text) {
         super(title_text);
         
-        try {
-            Droppable d = new Droppable(background,this);            
-        } catch (NodeAlreadyDroppableException e) {
-            // ...
+        // For each Propp function add a PlaceHolder to the grid node. Keep
+        // references to all these placeholders in placeholders.
+        // TODO: implement PlaceHolder class and use it here instead of disabled
+        // story cards.
+        for (int i=0; i<31; i++) {
+            StoryCard disabled = new StoryCard(""+i,""+i);
+            disabled.disable();
+            addToGrid(disabled.getNode());
+            placeholders.add(disabled);
         }        
     }
-    
-    public boolean dropped_onto(DropEvent de) {
-        StoryCard s = (StoryCard) de.getDraggee().getNode().getAttribute("StoryCard");
-        if (s == null) { return false; }
-        add(s.getNode());
-        storycards.add(s);
-        de.getDraggee().attach(this);
-        return true;
+
+    /**
+     * Return the distance between two points.
+     */
+    private double findDistance(double x1, double y1, double x2, double y2) {
+        double a = (x2-x1)*(x2-x1);
+        double b = (y2-y1)*(y2-y1);
+        return Math.sqrt(a+b);
     }
     
+    /**
+     * Return the offset of any pnode in global coords.
+     */
+    private Point2D globalPos(PNode n) {
+        return n.localToGlobal(n.parentToLocal(n.getOffset()));
+    }
+    
+    /**
+     * Return the nearest PlaceHolder in placeholders to the given StoryCard.
+     */
+    private StoryCard findNearest(StoryCard s) {
+        // First translate the storycard's x and y offsets to global coords.        
+        Point2D p2d = globalPos(s.getNode());
+        double x1 = p2d.getX();
+        double y1 = p2d.getY();
+        double nearest_distance = -1;
+        StoryCard nearest_placeholder = null;        
+        for (StoryCard p : placeholders) {
+            boolean taken = false;
+            for (StoryCard c : storycards) {
+                if (c.getNode().getOffset().equals(p.getNode().getOffset())) {
+                    // This placeholder already has a node.
+                    taken = true;
+                    break;
+                }
+            }
+            if (taken) { continue; }
+            p2d = globalPos(p.getNode());
+            double x2 = p2d.getX();
+            double y2 = p2d.getY();            
+            double distance = findDistance(x1,y1,x2,y2);
+            if (nearest_distance == -1 || distance < nearest_distance) {
+                nearest_distance = distance;
+                nearest_placeholder = p;
+            }
+        }
+        return nearest_placeholder;
+    }
+    
+    /**
+     * Return a StoryCard from storycards that has the same function as s, or
+     * null if no such story card exists.
+     */
+    private StoryCard findStoryCard(StoryCard s) {
+        for (StoryCard c : storycards) {
+            if (s.getTitle().equals(c.getTitle())) {
+                return c;
+            }
+        }
+        return null;
+    }    
+    
+    /**
+     * Called when a node is dropped onto this story map. Accept the node only
+     * if:
+     * 
+     * +   It has a StoryCard attribute attached to it.
+     * +   Field storycards does not already contain a story card with the same
+     *     function.
+     * +   There's at least one empty space: we have more placeholders than
+     *     story cards.
+     * 
+     * If the story card is accepted then add it to storycards, subscribe to its
+     * draggable instance, position the story card on top of the nearest free
+     * placeholder, and return true to indicate the drop was accepted.
+     */
+    @Override
+    public boolean dropped_onto(DropEvent de) {
+
+        if (placeholders.size() <= storycards.size()) {
+            // We have no space, reject the drop.
+            return false;
+        }
+        
+        StoryCard s = (StoryCard)
+                      de.getDraggee().getNode().getAttribute("StoryCard");
+                        
+        if (s == null) { 
+            // This object is not a story card, reject it.
+            return false;
+        }
+        
+        if (findStoryCard(s) != null) { 
+            // We already have a story card like this one, reject it.
+            return false;
+        }
+                
+        // Accept the new story card...
+        storycards.add(s);
+        // Subscribe to the Draggable of this story card.
+        Draggable d = (Draggable) s.getNode().getAttribute("Draggable");            
+        d.attach(this);
+        // Position the story card over the nearest free placeholder.
+        StoryCard nearest = findNearest(s);        
+        s.unhighlight();
+        addToOverlay(s.getNode());
+        s.getNode().setOffset(nearest.getNode().getOffset());
+        return true;
+    }
+
+    /**
+     * Called when a draggable that this story map is subscribed to is dropped
+     * onto something. Get the StoryCard that the Draggable instance belongs to
+     * and remove it from storycards, then return false to unsubscribe from the
+     * Draggable instance.
+     */
     public boolean notify(DropEvent de) {
+        /* Note: this method is tightly coupled to method dropped_onto above!
+         * 
+         * Here we don't check whether the droppee belonged to this StoryCards
+         * object (i.e. a StoryCard was dragged from this StoryCards then
+         * dropped back onto this StoryCards) because if that was the case then
+         * dropped_onto above would not have accepted the drop and this method
+         * would not be getting called.
+         */
+        Draggable draggee = de.getDraggee();
+        Droppable droppee = de.getDroppee();
+        StoryCard s = (StoryCard) draggee.getNode().getAttribute("StoryCard");
+        storycards.remove(s);
         return false;
     }   
 }
--- storymaps/StoryMapDemo.java
+++ storymaps/StoryMapDemo.java
@@ -1,13 +1,12 @@
 package storymaps;
+
+import DragAndDrop.*;
 import edu.umd.cs.piccolo.PCamera;
 import edu.umd.cs.piccolo.PCanvas;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
-import edu.umd.cs.piccolo.event.PDragEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
-import edu.umd.cs.piccolo.util.PPickPath;
 import edu.umd.cs.piccolox.PFrame;
-import java.awt.geom.Point2D;
 
 /**
  * A demo class for the StoryMap class.
@@ -16,88 +15,56 @@ import java.awt.geom.Point2D;
  */
 public class StoryMapDemo extends PFrame implements Receiver {
 
-    private VerticalLayoutNode home;
-    private StoryMap bank;
-    private StoryMap map;
+    private VerticalLayoutNode home = new VerticalLayoutNode(50);
+    private StoryMap map;    
     
     // Override PFrame's initialize method to run the demo.
     @Override
     public void initialize() {
-        home = new VerticalLayoutNode(50);
-        getCanvas().getLayer().addChild(home);
+        setSize(1024, 768);
         
-        bank = new StoryMap("Story Cards");
-        for (int i=0; i<30; i++) {
-            StoryCard card = new StoryCard("Argh", "Aaaargh!");
-            bank.addStoryCard(card);
-        }
-        home.addChild(bank.getNode());
-        
-        map = new StoryMap("My Story Map");
-        home.addChild(map.getNode());
+        PCanvas canvas = getCanvas();
+        canvas.getLayer().addChild(home);
 
-        final PCamera cam = getCanvas().getCamera();
+        map = new StoryMap("Story Map");
+        final PNode node = map.getRoot();        
+        home.addChild(map.getRoot());        
+        
+        DroppableRectangle rect = new DroppableRectangle(2000,400);
+        home.addChild(rect.getNode());
+        
+        for (int i=0; i<10; i++) {
+            StoryCard s = new StoryCard(""+i,""+i);
+            try {
+                Draggable d = new Draggable(s.getNode());
+            } catch (NodeAlreadyDraggableException e) {
+                // ...
+            }
+            rect.getNode().addChild(s.getNode());
+        }                
+                
+        // Remove the default event handler that enables panning with the mouse.    
+        canvas.removeInputEventListener(canvas.getPanEventHandler());
+        
+        final PCamera cam = canvas.getCamera();
+        cam.animateViewToCenterBounds(home.getGlobalFullBounds(), true, 750);
+        
+        // Make middle mouse button return camera to home position.
         cam.addInputEventListener(new PBasicInputEventHandler() { 		                    
-            // Make the camera zoom out when RMB is pressed.
             @Override
             public void mousePressed(PInputEvent event) {
                 if (event.getButton() == 2) {
                     cam.animateViewToCenterBounds(home.getGlobalFullBounds(), true, 750);
                 }
             }
-        });        
-                        
-        Messager m = Messager.getMessager();
-        m.accept("StoryCard clicked", this, null);
+        });
         
-        // Remove the default event handler that enables panning with the mouse.    
-        PCanvas canvas = getCanvas();
-        canvas.removeInputEventListener(canvas.getPanEventHandler());
-
-        // Create a new drag-and-drop event handler...
-        PDragEventHandler dragEventHandler =
-              new PDragEventHandler() {
-
-            private Point2D startPos;
-            
-            @Override
-            protected void startDrag(PInputEvent e) {
-                super.startDrag(e);
-                startPos = getDraggedNode().getOffset();
-            }
-
-            @Override
-            protected void endDrag(PInputEvent e) {
-                PPickPath path = e.getInputManager().getMouseOver();
-                PNode dropTarget = path.getPickedNode();
-                while (dropTarget == getDraggedNode()) {
-                    dropTarget = path.nextPickedNode();
-                }
-                StoryMap storymap = (StoryMap) dropTarget.getAttribute(StoryMap.attribute);
-                if (storymap == null) {
-                    getDraggedNode().setOffset(startPos);
-                }
-                else {
-                    StoryCard s = (StoryCard) getDraggedNode().getAttribute(StoryCard.attribute);
-                    if (s == null) {
-                        System.out.println("Dragged something that wasn't a StoryCard?");               
-                    }
-                    else {
-                        storymap.addStoryCard(s);
-                    }
-                }                
-                super.endDrag(e);
-            }
-        };
-        dragEventHandler.setMoveToFrontOnPress(true);        
-        // ...and add it to the canvas.
-        canvas.addInputEventListener(dragEventHandler);        
+        // Listen for 'clicked' messages from story cards (the receive method
+        // will be called), this is how we make RMB zoom in on cards.
+        Messager m = Messager.getMessager();
+        m.accept("StoryCard clicked", this, null);        
     }
-    
-    public static void main(String args[]) {
-        StoryMapDemo demo = new StoryMapDemo();
-    }
-    
+
      public void receive(String name, Object receiver_arg, Object sender_arg) {
          if (name.equals("StoryCard clicked")) {
             PCamera cam = getCanvas().getCamera();
@@ -105,6 +72,9 @@ public class StoryMapDemo extends PFrame implements Receiver {
             PNode node = card.getNode();
             cam.animateViewToCenterBounds(node.getGlobalBounds(), true, 750);
          }
-     }
+     }    
+    
+    public static void main(String args[]) {
+        StoryMapDemo demo = new StoryMapDemo();
+    }
 }
-
