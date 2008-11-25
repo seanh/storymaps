3352c4e9d3ce7c2962cbb269af318b330e1be93f
Sean Hammond <seanh@sdf.lonestar.org>
2008-11-25 14:09:41 +0000

    More work on swing, still pretty rough around the edges, but have the gui looking roughly how I wanted and the piccolo canvas embedded in the gui.
    	modified:   Swing.java

--- storymaps/Swing.java
+++ storymaps/Swing.java
@@ -1,5 +1,10 @@
 package storymaps;
 
+import edu.umd.cs.piccolo.PCamera;
+import edu.umd.cs.piccolo.PCanvas;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -8,23 +13,131 @@ import javax.swing.*;
  *
  * @author seanh
  */
-public class Swing {
+public class Swing implements Receiver {
     
     private JFrame frame;
+
+    private PCanvas canvas;
+    
+    /**
+     * The home node, to which all other nodes are attached.
+     */
+    private VerticalLayoutNode home = new VerticalLayoutNode(50);
+    
+    /**
+     * The collection of story cards that the user chooses from.
+     */
+    private StoryCards cards;
+    
+    /**
+     * The story map where the user places and arranges her chosen cards.
+     */
+    private StoryMap map;    
+    
+    private HelpText help_text = new HelpText();
+    
     
     public Swing() {
         makeFrame();
     }
 
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected static ImageIcon createImageIcon(String path, String description)
+    {
+        return new ImageIcon(path, description);
+    }
+        
     private void makeFrame() {
-        frame = new JFrame("StoryMaps");
+        frame = new JFrame("StoryMaps");        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
         Container contentPane = frame.getContentPane();
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
+                            
+        initializePCanvas();
+        contentPane.add(canvas);
+                
+        JPanel documentPanel = new JPanel();
+        documentPanel.setLayout(new BoxLayout(
+                                    documentPanel, BoxLayout.PAGE_AXIS));
+        JScrollPane scrollPane = new JScrollPane(documentPanel);
+        contentPane.add(scrollPane);
+                
+        for (Function f : Functions.getFunctions()) {
+            documentPanel.add(makeFunctionEditor(f));
+        }        
         
-        contentPane.setLayout(new BorderLayout());
+        makeMenu();
         
-        JLabel label = new JLabel("Ywan");
-        contentPane.add(label,BorderLayout.NORTH);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    
+    public void initializePCanvas() {
+
+        canvas = new PCanvas();
+        canvas.setPreferredSize(new Dimension(800,600));
+        canvas.setMinimumSize(new Dimension(800,600));
+        canvas.setBackground(Color.DARK_GRAY);
+                
+        canvas.getLayer().addChild(home);
+
+        cards = new StoryCards("Choose the Story Cards you want from here...");
+        home.addChild(cards.getNode());
         
+        map = new StoryMap("... and arrange them into your own Story Map here.");        
+        home.addChild(map.getNode());        
+                                
+        // Remove the default event handler that enables panning with the mouse.    
+        canvas.removeInputEventListener(canvas.getPanEventHandler());
+        
+        final PCamera cam = canvas.getCamera();
+        cam.animateViewToCenterBounds(home.getGlobalFullBounds(), true, 750);
+        
+        // Make middle mouse button return camera to home position.
+        cam.addInputEventListener(new PBasicInputEventHandler() { 		                    
+            @Override
+            public void mousePressed(PInputEvent event) {
+                if (event.getButton() == 3) {
+                    cam.animateViewToCenterBounds(home.getGlobalFullBounds(), true, 750);
+                }
+            }
+        });
+        
+        // Listen for 'clicked' messages from story cards (the receive method
+        // will be called), this is how we make RMB zoom in on cards.
+        Messager m = Messager.getMessager();
+        m.accept("StoryCard clicked", this, null);
+                        
+        cam.addChild(help_text.getNode());
+        help_text.getNode().setOffset(1024/2f,768/2f);
+        help_text.show("Welcome to the Story Maps application!\nLeft-click to drag,\ndouble-click to zoom in,\nright-click to zoom out.");        
+    }
+
+     public void receive(String name, Object receiver_arg, Object sender_arg) {
+         if (name.equals("StoryCard clicked")) {
+            PCamera cam = canvas.getCamera();
+            StoryCard card = (StoryCard) sender_arg;
+            PNode node = card.getNode();
+            cam.animateViewToCenterBounds(node.getGlobalBounds(), true, 750);
+         }
+     }    
+              
+    private JPanel makeFunctionEditor(Function f) {
+        JPanel container = new JPanel();
+        FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);
+        container.setLayout(flowLayout);
+        ImageIcon icon = createImageIcon(f.image,"Illustation for function.");
+        JLabel label = new JLabel(icon);
+        container.add(label);
+        JTextArea textArea = new JTextArea(7,66);
+        JScrollPane scrollPane = new JScrollPane(textArea);                    
+        container.add(scrollPane);
+        return container;
+    }
+
+    private void makeMenu() {
         JMenuBar menubar = new JMenuBar();
         frame.setJMenuBar(menubar);
         
@@ -53,12 +166,9 @@ public class Swing {
                 showAbout();
             }
         });
-        helpMenu.add(aboutItem);
-        
-        frame.pack();
-        frame.setVisible(true);
+        helpMenu.add(aboutItem);        
     }
-
+    
     private void openFile() {
         
     }
