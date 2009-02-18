db7ff630eab6e541ed988a3f065e64017775e782
seanh <seanh@sdf.lonestar.org>
2009-02-18 18:16:22 +0000

    Add an icon and a setIcon method to the Button class, have the WriteButton class use up and down arrows when collapsed and uncollapsed.
    Add arrow SVGs and PNGs to the data folder.
    
    	new file:   storymaps/data/gtk-go-down.png
    	new file:   storymaps/data/gtk-go-down.svg
    	new file:   storymaps/data/gtk-go-up.png
    	new file:   storymaps/data/gtk-go-up.svg
    	modified:   storymaps/ui/Button.java
    	modified:   storymaps/ui/WriteStoryButton.java

--- storymaps/ui/Button.java
+++ storymaps/ui/Button.java
@@ -4,11 +4,9 @@ import storymaps.Messager;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
-import edu.umd.cs.piccolo.util.PPaintContext;
-import java.awt.Graphics2D;
-
 
 /**
  * A button (clickable PNode) in Piccolo.
@@ -27,10 +25,16 @@ public class Button extends PNode {
      */
     private PText text;
     
+    /**
+     * The icon that is displayed on the button.
+     */
+    private PImage icon;
+    
+    private PNode text_and_icon = new PNode();    
     private PPath outer_rect;
     private PPath inner_rect;
         
-    public Button(String name) {
+    public Button(String name, String text, PImage icon) {
         this.name = name;
         
         addInputEventListener(new PBasicInputEventHandler() {
@@ -40,41 +44,59 @@ public class Button extends PNode {
             }
         });
         
-        text = new PText(name);
-        center_text();
-        
+        this.text = new PText(text);
+        text_and_icon.addChild(this.text);
+        setIcon(icon);        
+                
         // Draw two rectangles around the text so it looks like a button.
-        float w = (float)text.getWidth() + 7.5f;
-        float h = (float)text.getHeight() + 7.5f;        
+        float w = (float)text_and_icon.getFullBoundsReference().getWidth() + 7.5f;
+        float h = (float)text_and_icon.getFullBoundsReference().getHeight() + 7.5f;        
         outer_rect = PPath.createRoundRectangle(-w/2f,-h/2f,w,h,10,10);
         this.addChild(outer_rect);
         inner_rect = PPath.createRoundRectangle(-w/2f+2,-h/2f+2,w-4,h-4,6,6);
         this.addChild(inner_rect);
-                
-        this.addChild(text);        
+                            
+        this.addChild(text_and_icon);
     }
 
     /**
      * Center the text node's geometry at (0,0) in its local coord space/
      */
-    private void center_text() {
-        text.setOffset(-text.getWidth()/2f,-text.getHeight()/2f);
+    private void center_text_and_icon() {
+        // Position the icon just to the right of the text.
+        icon.setOffset(text.getOffset().getX()+text.getWidth(),text.getOffset().getY());
+        // Position the icon and text in the centre of the background rects.
+        double width = text_and_icon.getFullBounds().getWidth();
+        double height = text_and_icon.getFullBounds().getHeight();
+        text_and_icon.setOffset(-width/2f,-height/2f);
     }
     
     protected void clicked() {
         Messager.getMessager().send("button clicked",name);
     }
-    
+
+    public String getName() {
+        return name;
+    }
+        
     public void setText(String text) {
         this.text.setText(text);
-        center_text();
+        center_text_and_icon();
     }
 
     public String getText() {
         return text.getText();
     }
-    
-    public String getName() {
-        return name;
-    }
+        
+    public void setIcon(PImage icon) {
+        if (this.icon != null) {
+            text_and_icon.removeChild(this.icon);
+        }        
+        // Whatever the original size of the icon, scale it so that it is the
+        // same height as the text.        
+        icon.setScale(text.getHeight()/icon.getHeight());
+        text_and_icon.addChild(icon);
+        this.icon = icon;
+        center_text_and_icon();
+    }    
 }
--- storymaps/ui/WriteStoryButton.java
+++ storymaps/ui/WriteStoryButton.java
@@ -1,6 +1,7 @@
 package storymaps.ui;
 
-import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolo.nodes.PImage;
+import storymaps.ResourceLoader;
 
 /**
  * A button named "Write Story" that displays some text and changes between two
@@ -14,10 +15,12 @@ public class WriteStoryButton extends Button {
 
     private static final String COLLAPSED_TEXT = "Write Story";
     private static final String UNCOLLAPSED_TEXT = "Go Back";
+    private static final PImage COLLAPSED_ICON = new PImage(ResourceLoader.loadImage("/storymaps/data/gtk-go-down.png"));
+    private static final PImage UNCOLLAPSED_ICON = new PImage(ResourceLoader.loadImage("/storymaps/data/gtk-go-up.png"));
     
     public WriteStoryButton() {
-        super("Write Story");
-        setScale(10);
+        super("Write Story","Write Story",COLLAPSED_ICON);        
+        setScale(10);        
     }
     
     @Override
@@ -25,8 +28,10 @@ public class WriteStoryButton extends Button {
         super.clicked();
         if (getText().equals(COLLAPSED_TEXT)) {
             setText(UNCOLLAPSED_TEXT);
+            setIcon(UNCOLLAPSED_ICON);
         } else {
             setText(COLLAPSED_TEXT);
+            setIcon(COLLAPSED_ICON);
         }
     }    
 }
