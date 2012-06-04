77876cb2414c222e079ce97b4a6b16ef9239e08a
Sean Hammond <seanhammond@lavabit.com>
2012-06-04 10:52:26 +0200

    Add copyright license headers

--- src/DragAndDrop/Draggable.java
+++ src/DragAndDrop/Draggable.java
@@ -1,3 +1,23 @@
+
+/* 
+    Copyright: (c) 2006-2012 Sean Hammond <seanhammond@lavabit.com>
+
+    This file is part of Storymaps.
+
+    Storymaps is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Storymaps is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Storymaps.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 // FIXME: only one event handler, attached to the canvas, is needed to implement
 // drag & drop, not one event handler per draggable instance attached to the
 // draggable node. The event handler could be a static field. But how to access
